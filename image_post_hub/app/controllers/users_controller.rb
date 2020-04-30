541022fbb412c579a2c13939260b32b7a8b4392e
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-05-01 08:12:42 +0900

    #5 詳細画面の作成

--- image_post_hub/app/controllers/users_controller.rb
+++ image_post_hub/app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -8,12 +10,18 @@ class UsersController < ApplicationController
     if @user.save
     else
       render :new
+      redirect_to user_path(@user.id)
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
   end
 end
