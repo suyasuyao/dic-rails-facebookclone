083fe8221055d47d18e55147f226f214a6352dfb
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-05-10 11:24:14 +0900

    #6 Picture機能の作成

--- image_post_hub/config/routes.rb
+++ image_post_hub/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
   get 'sessions/new'
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create show]
--- image_post_hub/db/schema.rb
+++ image_post_hub/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_052434) do
+ActiveRecord::Schema.define(version: 2020_05_10_004112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_02_29_052434) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
