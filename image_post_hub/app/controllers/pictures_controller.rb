5b51d909ae2488dc8827c09bafe413f3d7817b6d
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-05-11 04:25:44 +0900

    #6 不要なjson部分の処理を削除

--- image_post_hub/app/controllers/pictures_controller.rb
+++ image_post_hub/app/controllers/pictures_controller.rb
@@ -27,10 +27,8 @@ class PicturesController < ApplicationController
     respond_to do |format|
       if @picture.save
         format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,10 +39,8 @@ class PicturesController < ApplicationController
     respond_to do |format|
       if @picture.update(picture_params)
         format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -55,7 +51,6 @@ class PicturesController < ApplicationController
     @picture.destroy
     respond_to do |format|
       format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
